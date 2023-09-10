663e424006d4401429fc75ed6a11c5aac835cbf0
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-10 12:24:04 +0530

    v3

--- api/server.js
+++ api/server.js
@@ -2,9 +2,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
 const middlewares = jsonServer.defaults();
-require("cors").conig();
+
+const cors = require("cors");
 server.use(middlewares);
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
