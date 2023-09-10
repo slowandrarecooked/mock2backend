c57ad1c0545b0dbabfecb2f3db0eb78bac1d0cdc
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-10 12:39:35 +0530

    v4

--- api/server.js
+++ api/server.js
@@ -6,12 +6,6 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-server.use(cors(corsOptions));
 server.use(middlewares);
 
 // Add this before server.use(router)
